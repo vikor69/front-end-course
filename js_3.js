1b507294ba8b76282bd759ac060358fd7a8da904
vikor69 <bancevich.viktor@mail.ru>
2018-06-05 19:36:11 +0300

    Add files via upload

---
+++ js_3.js
@@ -0,0 +1,12 @@
+var arr = [1, 2, 3, '4', 'hello'];
+function isInArray(arr,el) { if (arr.join().indexOf('5')!=-1)
+                                                            {console.log('true')}
+                             else{console.log('false')};                             
+                             
+                           };
+                                                   
+                   
+
+
+      
+                      
